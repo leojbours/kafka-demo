34d8e5fce1a4ae75b0bd861a2c4417b34ee4abc4
Leonardo Bourscheid <leonardojohannbourscheid@gmail.com>
2026-08-02 16:42:24 -0300

    Adiciona str-producer

---
+++ strproducer/src/main/java/com/github/leojbours/strproducer/StrproducerApplication.java
@@ -0,0 +1,13 @@
+package com.github.leojbours.strproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StrproducerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(StrproducerApplication.class, args);
+  }
+
+}
